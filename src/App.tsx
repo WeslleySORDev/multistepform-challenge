b9d1c7e23b8e403e8ebdd4af63d10d5787aecd38
WeslleySORDev <weslleysordev@gmail.com>
2023-05-09 21:59:28 -0300

    add buttons to nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import bgSidebarMobile from "/assets/images/bg-sidebar-mobile.svg";
+
 function App() {
   return (
-    <>
-      <h1>AAA</h1>
-    </> 
-  )
+    <div className="flex flex-col">
+      <nav className="bg-[url(/assets/images/bg-sidebar-mobile.svg)] h-[172px] pt-8">
+        <div className="flex gap-4 w-fit mx-auto">
+          <button className="bg-primary-LightBlue font-medium h-8 w-8 rounded-full">1</button>
+          <button className="text-neutral-White border border-neutral-White font-medium h-8 w-8 rounded-full">2</button>
+          <button className="text-neutral-White border border-neutral-White font-medium h-8 w-8 rounded-full">3</button>
+          <button className="text-neutral-White border border-neutral-White font-medium h-8 w-8 rounded-full">4</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default App
+export default App;
